3d4c329aac651009177f6ae69458e8c7957760a7
countryqueen <countryqueen@users.noreply.github.com>
2017-09-11 20:42:13 -0500

    Please help
    Here is my project

---
+++ JonesProject2DataStructures/src/newpackage/TheStack.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/*‘***Class Name: TheStack
+‘*** Class Author: Angelica Jones
+‘*** Purpose of the class: I wrote this class to create the stack.
+‘*** Date: 9/09/17
+‘*** List of changes with dates. 
+‘*** Look at this!‘*** List all the places in the code where you did something interesting,‘*** clever or elegant.  If you did good work in this program and you want‘*** me to consider it in your grade, point it out here.‘*******************************************************/
+public class TheStack 
+{
+    
+
+    private int index;
+    //a private variable for the index aka the length of our stack array
+    
+    public TheStack(int maxSize)
+    {
+        
+    }
+/***  Method Name: push
+ * Method Author: Angelica
+ ‘*** Purpose: to push integers on the stack
+ ‘***Method Inputs:‘*** integer value
+ * Return value:void.
+ * Date* 9/11/17 */
+    public void push(int value)
+
+    {
+
+        if(index >0)
+
+    {
+
+       
+    }
+
+    }
+/***  Method Name: pop
+ * Method Author: Angelica
+ ‘*** Purpose: to pop integers off of the stack
+ ‘***Method Inputs:‘*** no parameters
+ * Return value:
+ * Date* 9/11/17 */
+
+
+    public int pop()
+
+    {
+
+      
+
+    {
+
+    
+    //if the index is greater than the max, then increment the index.
+
+    }
+        return -1;
+
+
+    }
+
+}
+    
